27399ce54379e87148d9fe23135fe7139e65c0f3
realMNohgee <clund25@gmail.com>
2022-05-23 11:50:32 -0500

    Switching it up.  Plan on using multiple classes and not use and private methods.  Will take more code but believe it reads better this way and makes the game play much easier to understand for the player. Added board_outline to class BoardOutline will change names so not so similar.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -13,12 +13,38 @@ require 'pry-byebug'
 # some way to exit game if wanted #
 # possibly set up a simple AI to play against when only one player is available #
 
-class TicTacToe
-  def initialize()
-  
+class BoardOutline
+  $valid_moves = %w[1 2 3 4 5 6 7 8 9]
+  attr_accessor :position_one, :position_two, :position_three, :position_four, 
+  :position_five, :position_six, :position_seven, :position_eight, :position_nine
+
+  def initialize(p1, p2, p3, p4, p5, p6, p7, p8, p9)
+    @position_one = p1
+    @position_two = p2
+    @position_three = p3
+    @position_four = p4
+    @position_five = p5
+    @position_six = p6
+    @position_seven = p7
+    @position_eight = p8
+    @position_nine = p9
   end
 
-  def switch_player
+
+  def board_outline
+    puts "
+    #{@position_one} | #{@position_two} | #{@position_three}
+    --+---+--
+    #{@position_four} | #{@position_five} | #{@position_six}
+    --+---+--
+    #{@position_seven} | #{@position_eight} | #{@position_nine}
+    
+    "
+
+  end
+end
+  
+class 
   
   end
 
@@ -30,10 +56,6 @@ class TicTacToe
 
   def player_move(row, column)
   
-  end
-
-  def board_outline
-
   end
 
   def game_play()
