d944d897dbff3ec7d7f711a4e4107cee17f9f91c
realMNohgee <clund25@gmail.com>
2022-05-23 10:02:04 -0500

    simple outline of what I would like to do with my tic-tac-toe game and put my pry-byebug on line one so I would not forget

---
+++ tic_tac_toe.rb
@@ -0,0 +1,13 @@
+require 'pry-byebug'
+
+# pseudocode #
+# take players_one and players_two names #
+# ask players what symbols they want to use #
+# design game_board #
+# winning combinations #
+# some type of loop (infinte loop?) #
+# if board is full game is tied #
+# if tied ask to play again? #
+# if there is a winner, congratulations are in order #
+# some way to exit game if wanted #
+# possibly set up a simple AI to play against when only one player is available #
