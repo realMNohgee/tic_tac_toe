0a380026a4ea63cf7769ca2c64b793765cf5c3e3
realMNohgee <clund25@gmail.com>
2022-05-23 13:15:41 -0500

    Added player two way to move to corresponding position by choosing a number from the board outline.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -66,6 +66,7 @@ def start_game
   $board.draw_outline
 end
 
+# Setting how the player will place their marker #
 # Will set invalid choices into this next method(s) #
 
 def player_one_moves
@@ -104,6 +105,46 @@ def player_one_moves
     $valid_moves.delete('8')
   when '9'
     $board.position_nine = 'X'
+    $valid_moves.delete('9')
+  end
+end
+
+def player_two_moves
+  puts "#{$player_two.name}: Please choose an available space to place your X by selecting the corresponding number."
+  $move = gets.chomp
+
+  until $valid_moves.include? $move
+    puts 'Invalid move you big dummy! Just kidding!  Will you please try agian?'
+    $move = gets.chomp
+  end
+
+  case $move
+  when '1'
+    $board.position_one = 'O'
+    $valid_moves.delete('1')
+  when '2'
+    $board.position_two = 'O'
+    $valid_moves.delete('2')
+  when '3'
+    $board.position_three = 'O'
+    $valid_moves.delete('3')
+  when '4'
+    $board.position_four = 'O'
+    $valid_moves.delete('4')
+  when '5'
+    $board.position_five = 'O'
+    $valid_moves.delete('5')
+  when '6'
+    $board.position_six = 'O'
+    $valid_moves.delete('6')
+  when '7'
+    $board.position_seven = 'O'
+    $valid_moves.delete('7')
+  when '8'
+    $board.position_eight = 'O'
+    $valid_moves.delete('8')
+  when '9'
+    $board.position_nine = 'O'
     $valid_moves.delete('9')
   end
 end
